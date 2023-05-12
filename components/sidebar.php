49ef5a665b67c770c511c2d9c74d6d1bcb2fe8b4
zuhalcode <zuhalcode@gmail.com>
2023-05-12 15:32:27 +0700

    fixing dashboard UI

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,81 +1,81 @@
 <!--**********************************
             Sidebar start
         ***********************************-->
-        <div class="dlabnav">
-			<div class="dlabnav-scroll">	
-				<center class="profile-photo">
-				<img src="images/profile/profile.png" class="img-fluid rounded-circle" alt="">
-				</center>
-				<div class="profile-name px-3 pt-2">
-					<h4 align= "center" class="text-primary mb-0">Admin</h4>
-					
-				</div>
-				<ul class="metismenu" id="menu">
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-							<i class="material-icons-outlined">dashboard</i>
-							<span class="nav-text">Dashboard</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-						<i class="material-icons">paid</i>	
-						<span class="nav-text">Transaksi</span>
-					</a>
-					</li>
-					<li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
-						<i class="material-icons">
+<div class="dlabnav">
+	<div class="dlabnav-scroll">
+		<center class="profile-photo mt-2">
+			<img src="images/profile/profile.png" class="img-fluid rounded-circle" alt="">
+		</center>
+		<div class="profile-name px-3 pt-2">
+			<h4 align="center" class="text-primary mb-0">Admin</h4>
+
+		</div>
+		<ul class="metismenu" id="menu">
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons-outlined">dashboard</i>
+					<span class="nav-text">Dashboard</span>
+				</a>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons">paid</i>
+					<span class="nav-text">Transaksi</span>
+				</a>
+			</li>
+			<li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons">
 						settings
-						</i>
-						<span class="nav-text">FDS Configuration</span>
-					</a>
-					<ul aria-expanded="false">
-						<li><a href="content.html">Action</a></li>
-						<li><a href="menu.html">Available Fields</a></li>	
-						<li><a href="email-template.html">Rules</a></li>		
-						<li><a href="blog.html">Black List</a></li>			
-					</ul>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-						<i class="material-icons"> report </i>
-						<span class="nav-text">Report</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-						<i class="material-icons"> key </i>
-							<span class="nav-text">Change Password</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-							
-							<i class="material-icons"> person </i>
-							<span class="nav-text">User Mgmt</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-							<i class="material-icons"> search </i>
-							<span class="nav-text">Audit Trail</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-							<i class="material-icons"> help </i>
-							<span class="nav-text">Helpdesk</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-							<i class="material-icons"> folder </i>
-							<span class="nav-text">Documentation</span>
-						</a>
-					</li>
-					<li><a class=" " href="javascript:void(0);" aria-expanded="false">
-							<i class="material-icons"> logout </i>
-							<span class="nav-text">Logout</span>
-						</a>
-					</li>
-					<div class="copyright">
-						<p align="center">© 2023 APLINK</p>
-					</div>
+					</i>
+					<span class="nav-text">FDS Configuration</span>
+				</a>
+				<ul aria-expanded="false">
+					<li><a href="content.html">Action</a></li>
+					<li><a href="menu.html">Available Fields</a></li>
+					<li><a href="email-template.html">Rules</a></li>
+					<li><a href="blog.html">Black List</a></li>
 				</ul>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons"> report </i>
+					<span class="nav-text">Report</span>
+				</a>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons"> key </i>
+					<span class="nav-text">Change Password</span>
+				</a>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+
+					<i class="material-icons"> person </i>
+					<span class="nav-text">User Mgmt</span>
+				</a>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons"> search </i>
+					<span class="nav-text">Audit Trail</span>
+				</a>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons"> help </i>
+					<span class="nav-text">Helpdesk</span>
+				</a>
+			</li>
+			<li><a class="" href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons"> folder </i>
+					<span class="nav-text">Documentation</span>
+				</a>
+			</li>
+			<li><a class=" " href="javascript:void(0);" aria-expanded="false">
+					<i class="material-icons"> logout </i>
+					<span class="nav-text">Logout</span>
+				</a>
+			</li>
+			<div class="copyright">
+				<p align="center">© 2023 APLINK</p>
 			</div>
-        </div>
-        <!--**********************************
+		</ul>
+	</div>
+</div>
+<!--**********************************
             Sidebar end
         ***********************************-->
